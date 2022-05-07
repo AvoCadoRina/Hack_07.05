ea55d4693e22c7c98c2d6b149601c7794739910d
Ekaterina <78584614+AvoCadoRina@users.noreply.github.com>
2022-05-07 15:49:28 +0300

    Delete sort_time.js

--- sort_time.js
+++
@@ -1,8 +0,0 @@
-const persons = [
-    { name: "Ivan", date: new Date('2019-01-22') },
-    { name: "Mark", date: new Date('2022-01-05') },
-    { name: "Lidia", date: new Date('2022-05-22') },
-]
-const sorted = persons.sort((a, b) => b.date - a.date)
-
-console.log(sorted);
